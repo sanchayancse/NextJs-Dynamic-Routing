1e04fa51434ab34a0a72b3ad8e8176097ab5bcef
Sanchayan Das <45485694+sanchayancse@users.noreply.github.com>
2020-11-07 09:27:53 +0530

    All file added

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,10 +1,14 @@
 import { useRouter } from "next/router";
-import posts from '../post.json'
+import  posts from '../../post.json'
+
 
 export default () => {
   const router = useRouter();
   const post = posts[router.query.id];
+
   if (!post) return <p></p>;
+
+  
   return (
     <>
       <h1>{post.title}</h1>
